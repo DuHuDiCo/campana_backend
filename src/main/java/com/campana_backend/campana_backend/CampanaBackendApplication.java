52ac109bee4011051c7e90faf0bd7edbc3f4d245
Duvan Diaz <dudicodiaz@gmail.com>
2023-03-09 11:03:08 -0500

    first commit

---
+++ src/main/java/com/campana_backend/campana_backend/CampanaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campana_backend.campana_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampanaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campana_backend/campana_backend/CampanaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campana_backend.campana_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampanaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
